1ac08db9abfd7cec5e8de382b4966f0bee170283
Sonja Stefani <sonja.stefani@yahoo.de>
2018-03-01 14:07:02 +0100

    Fixed some issues in GraphVisualizer

--- KnowledgeVisualizationVR/Assets/GraphVisualizer.cs
+++ KnowledgeVisualizationVR/Assets/GraphVisualizer.cs
@@ -10,14 +10,14 @@ public class GraphVisualizer : MonoBehaviour {
     public GameObject ball;
 
     //THESE VALUES NEED TO BE SET TO SPECIFIC VALUES AND MADE PRIVATE
-    public float DEFAULT_LENGTH = 0.0f;
-    public float DEFAULT_WIDTH = 0.0f;
-    public float DEFAULT_HEIGHT = 0.0f;
-    public float DEFAULT_VOLUME = 0.0f; //IMPORTANT: WHEN VALUES FOUND, SET THIS TO DEFAULT VOLUME
-    public float DEFAULT_CUBE_ROOT = 0.0f; //IMPORTANT: WHEN VALUES FOUND, SET THIS TO DEFAULT CUBE ROOT
+    public float DEFAULT_LENGTH = 100.0f;
+    public float DEFAULT_WIDTH = 100.0f;
+    public float DEFAULT_HEIGHT = 100.0f;
+    public float DEFAULT_VOLUME = 1000000.0f; //IMPORTANT: WHEN VALUES FOUND, SET THIS TO DEFAULT VOLUME
+    public float DEFAULT_CUBE_ROOT = 100.0f; //IMPORTANT: WHEN VALUES FOUND, SET THIS TO DEFAULT CUBE ROOT
 
     public float DEFAULT_STEP = 0.10f;
-    public int NUMBER_OF_ITERATIONS = 100;
+    public int NUMBER_OF_ITERATIONS = 50;
 
     private Graph graph;
     private float volume; //this defines the boundaries of the drawn Graph
@@ -36,10 +36,11 @@ public class GraphVisualizer : MonoBehaviour {
 
     public void Start()
     {
+        /*
         communicator = new WebHandler();
         //The string "Graphentheorie" is merely a teststring
         StartCoroutine(communicator.requestContent(message + "Graphentheorie"));
-        /*
+        
         string[] content = communicator.getSectionedContent();
         for (int i = 0; i < content.Length; i++)
         {
@@ -48,27 +49,36 @@ public class GraphVisualizer : MonoBehaviour {
         */
 
         /* This is for testing
-         * 
-        Graph currentGraph = new Graph();
-        for (int i = 0; i < 50; i++)
-        {
-            Graph.Information newInfo = new Graph.Information();
-            Graph.Node newNode = new Graph.Node(newInfo);
-            newNode.setPosition((float) i, (float) i, (float) i);
-            Debug.Log(newNode.getX() + ", " + newNode.getY() + ", " + newNode.getZ());
-            currentGraph.addNode(newNode);
-        }
-        setGraph(currentGraph, 20.0f, 20.0f, 20.0f);
-        Debug.Log(boundaries.width + ", " + boundaries.length + ", " + boundaries.height);
-        Debug.Log(volume);
-        Debug.Log(optimalDistance);
-        FruchtermanReingold();
-        drawGraph();
-        */
+         */
+        temperature = 5.0f;
+        makeStar();
     }
 
+    public void makeStar()
+    {
+        Graph newGraph = new Graph();
+        Graph.Node newNode = new Graph.Node(null);
+        newNode.setPosition(0.0f, 0.0f, 0.0f);
+        newGraph.addNode(newNode);
+        for (int i = 0; i < 49; i++)
+        {
+            Graph.Node nextNode = new Graph.Node(null);
+            nextNode.setPosition(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+            Graph.Edge edge = new Graph.Edge(newNode, nextNode);
+
+            newGraph.addNode(nextNode);
+            newGraph.addEdge(edge);
+        }
+
+        setGraph(newGraph);
+    }
+
+    float time = 0;
+    int counter = 0;
+    List<GameObject> drawn;
     public void Update()
     {
+        /*
         if (!communicator.getContent().Equals("") && printed == false)
         {
             string[] content = communicator.getSectionedContent();
@@ -78,21 +88,31 @@ public class GraphVisualizer : MonoBehaviour {
             }
             printed = true;
         }
+        */
+        time += Time.deltaTime;
+        if (time > 0.05f && counter <10000)
+        {
+            if (drawn != null) { foreach (GameObject obj in drawn)
+                {
+                    Destroy(obj);
+                }
+            }
+            FruchtermanReingold();
+            drawn = drawGraph();
+            counter++;
+            time = 0;
+        }
     }
 
-    public void setGraph()
-    {
-        graph = new Graph();
-        setBoundaries();
-        volume = DEFAULT_VOLUME;
-        optimalDistance = DEFAULT_CUBE_ROOT;
-    }
     public void setGraph(Graph graph)
     {
         this.graph = graph;
-        setBoundaries();
+        if (boundaries.height == 0 && boundaries.width == 0 && boundaries.length == 0)
+        {
+            setBoundaries();
+        }
         volume = DEFAULT_VOLUME;
-        optimalDistance = DEFAULT_CUBE_ROOT;
+        optimalDistance = Mathf.Pow(volume/graph.getNodes().Count, (1.0f/3.0f));
     }
 
     public void setGraph(Graph graph, float x, float y, float z)
@@ -100,7 +120,7 @@ public class GraphVisualizer : MonoBehaviour {
         this.graph = graph;
         setBoundaries(x, y, z);
         volume = boundaries.width * boundaries.length * boundaries.height; //get volume
-        optimalDistance = Mathf.Pow(volume, (1.0f / 3.0f)); //get cube root of volume
+        optimalDistance = Mathf.Pow(volume/graph.getNodes().Count, (1.0f / 3.0f)); //get cube root of volume
     }
 
     public void setBoundaries(float l, float w, float h)
@@ -125,6 +145,9 @@ public class GraphVisualizer : MonoBehaviour {
         return t - DEFAULT_STEP;
     }
 
+    float temperature;
+
+
     //this algorithm does NOT use the grid variant
     //it is a replica of the pseudocode given in the Fruchterman Reingold paper
     //this is a guideline for the grid based algorithm
@@ -134,17 +157,13 @@ public class GraphVisualizer : MonoBehaviour {
     {
         List<Graph.Node> nodes = graph.getNodes();
         List<Graph.Edge> edges = graph.getEdges();
-        float temperature = 10.0f;
+        //float temperature = 5.0f;
         for (int i = 0; i < NUMBER_OF_ITERATIONS; i++)
         {
             //this is the foreach for repulsive forces
             //it adds to the displacement of a node by calculating repulsive forces for each other node in the graph
             foreach (Graph.Node currentNode in nodes)
             {
-
-                Debug.Log("Before Repulsive" + currentNode.getX() + ", " + currentNode.getY() + ", " + currentNode.getZ());
-                Debug.Log(currentNode.getDisplacementX() + ", " + currentNode.getDisplacementY() + ", " + currentNode.getDisplacementZ());
-
                 currentNode.setDisplacement(0.0f, 0.0f, 0.0f);
                 //calculate forces for currentNode with all other nodes and add to displacement
                 foreach (Graph.Node node in nodes)
@@ -157,9 +176,9 @@ public class GraphVisualizer : MonoBehaviour {
                         float differenceZ = currentNode.getZ() - node.getZ();
 
                         //this is |difference|
-                        float differenceLength = Mathf.Sqrt(differenceX*differenceX +
-                            differenceY*differenceY +
-                            differenceZ*differenceZ);
+                        float differenceLength = Mathf.Sqrt(differenceX * differenceX +
+                            differenceY * differenceY +
+                            differenceZ * differenceZ);
 
                         //v.disp := v.disp + (difference / |difference|) * repForce(|difference|)
                         float repulsiveForce = getRepulsiveForce(differenceLength);
@@ -168,63 +187,88 @@ public class GraphVisualizer : MonoBehaviour {
                             (differenceZ / differenceLength) * repulsiveForce);
                     }
                 }
-                Debug.Log("After Repulsive in loop for current" + currentNode.getX() + ", " + currentNode.getY() + ", " + currentNode.getZ());
-                Debug.Log(currentNode.getDisplacementX() + ", " + currentNode.getDisplacementY() + ", " + currentNode.getDisplacementZ());
 
-                //this is the foreach for attractive forces
-                //similar to above but with edges
-                foreach (Graph.Edge edge in edges)
-                {
-                    Debug.Log("Reached edge loop");
-                    //difference := e.v.pos - e.u.pos
-                    float differenceX = edge.getFrom().getX() - edge.getTo().getX();
-                    float differenceY = edge.getFrom().getY() - edge.getTo().getY();
-                    float differenceZ = edge.getFrom().getZ() - edge.getTo().getZ();
+            }
 
-                    //|difference|
-                    float differenceLength = Mathf.Sqrt(differenceX * differenceX +
-                        differenceY * differenceY +
-                        differenceZ * differenceZ);
+            //this is the foreach for attractive forces
+            //similar to above but with edges
+            foreach (Graph.Edge edge in edges)
+            {
+                Debug.Log("Reached edge loop");
+                //difference := e.v.pos - e.u.pos
+                float differenceX = edge.getFrom().getX() - edge.getTo().getX();
+                float differenceY = edge.getFrom().getY() - edge.getTo().getY();
+                float differenceZ = edge.getFrom().getZ() - edge.getTo().getZ();
 
-                    float attractiveForce = getAttractiveForce(differenceLength);
+                //|difference|
+                float differenceLength = Mathf.Sqrt(differenceX * differenceX +
+                    differenceY * differenceY +
+                    differenceZ * differenceZ);
+
+                float attractiveForce = getAttractiveForce(differenceLength);
                     
-                    //e.v.disp := e.v.disp - (difference / |difference|) * attractiveForce(|difference|)
-                    edge.getFrom().setDisplacement(edge.getFrom().getDisplacementX() - (differenceX / differenceLength) * attractiveForce,
-                        edge.getFrom().getDisplacementY() - (differenceY / differenceLength) * attractiveForce,
-                        edge.getFrom().getDisplacementZ() - (differenceZ / differenceLength) * attractiveForce);
+                //e.v.disp := e.v.disp - (difference / |difference|) * attractiveForce(|difference|)
+                edge.getFrom().setDisplacement(edge.getFrom().getDisplacementX() - (differenceX / differenceLength) * attractiveForce,
+                    edge.getFrom().getDisplacementY() - (differenceY / differenceLength) * attractiveForce,
+                    edge.getFrom().getDisplacementZ() - (differenceZ / differenceLength) * attractiveForce);
 
-                    //e.u.disp := e.u.disp + (difference / |difference|) * attractiveForce(|difference)
-                    edge.getTo().setDisplacement(edge.getTo().getDisplacementX() - (differenceX / differenceLength) * attractiveForce,
-                       edge.getTo().getDisplacementY() - (differenceY / differenceLength) * attractiveForce,
-                       edge.getTo().getDisplacementZ() - (differenceZ / differenceLength) * attractiveForce);
+                //e.u.disp := e.u.disp + (difference / |difference|) * attractiveForce(|difference)
+                edge.getTo().setDisplacement(edge.getTo().getDisplacementX() - (differenceX / differenceLength) * attractiveForce,
+                    edge.getTo().getDisplacementY() - (differenceY / differenceLength) * attractiveForce,
+                    edge.getTo().getDisplacementZ() - (differenceZ / differenceLength) * attractiveForce);
+            }
+
+            //last foreach, limit displacement by boundaries and temperature
+
+            int wasauchimmer = 0;    
+            foreach (Graph.Node node in nodes)
+            {
+
+                if (float.IsNaN(node.getX()) || float.IsNaN(node.getY()) || float.IsNaN(node.getZ()) || float.IsNaN(node.getDisplacementX()) || float.IsNaN(node.getDisplacementY()) || float.IsNaN(node.getDisplacementZ()))
+                {
+                    Debug.Log("If Pos or Disp NaN then position: " + node.getX() + ", " + node.getY() + ", " + node.getZ() + "\n" +
+                        "Displacement: " + node.getDisplacementX() + ", " + node.getDisplacementY() + ", " + node.getDisplacementZ() + "\n" +
+                        "Temperatur: " + temperature + "\n" + 
+                        "Knotenindex: " + wasauchimmer);
+
+                    return;
+                }
+                float displacementLength = Mathf.Sqrt(Mathf.Pow(node.getDisplacementX(), 2.0f)+
+                    Mathf.Pow(node.getDisplacementY(), 2.0f)+
+                    Mathf.Pow(node.getDisplacementZ(), 2.0f));
+
+                Debug.Log("DisplacementLength: " + displacementLength);
+
+
+                //NOTE: displacement length?
+                float newPosX = node.getX() + Mathf.Min(displacementLength, temperature) * node.getDisplacementX() / displacementLength;
+                float newPosY = node.getY() + Mathf.Min(displacementLength, temperature) * node.getDisplacementY() / displacementLength;
+                float newPosZ = node.getZ() + Mathf.Min(displacementLength, temperature) * node.getDisplacementZ() / displacementLength;
+
+                if (float.IsNaN(newPosX) || float.IsNaN(newPosY) || float.IsNaN(newPosZ))
+                {
+                    Debug.Log("If newPos NaN then old Position: " + node.getX() + ", " + node.getY() + ", " + node.getZ() + "\nDisplacement: " + node.getDisplacementX() + ", " + node.getDisplacementY() + ", " + node.getDisplacementZ());
+                    Debug.Log("If newPos NaN then new Position: " + newPosX + ", " + newPosY + ", " + newPosZ);
                 }
 
-                //last foreach, limit displacement by boundaries and temperature
-                
-                foreach (Graph.Node node in nodes)
-                {
-
-                    float displacementLength = Mathf.Sqrt(Mathf.Pow(node.getDisplacementX(), 2.0f)+
-                        Mathf.Pow(node.getDisplacementY(), 2.0f)+
-                        Mathf.Pow(node.getDisplacementZ(), 2.0f));
-                    Debug.Log("DisplacementLength: ");
+                node.setPosition(newPosX,
+                    newPosY,
+                    newPosZ);
                     
-                    node.setPosition(node.getX() + (node.getDisplacementX() / displacementLength) * Mathf.Min(node.getDisplacementX(), temperature),
-                        node.getY() + (node.getDisplacementY() / displacementLength) * Mathf.Min(node.getDisplacementY(), temperature),
-                        node.getZ() + (node.getDisplacementZ() / displacementLength) * Mathf.Min(node.getDisplacementZ(), temperature));
-                    
-                    node.setPosition(Mathf.Min(boundaries.width/2.0f, Mathf.Max(-boundaries.width/2, node.getX())),
-                        Mathf.Min(boundaries.height/2.0f, Mathf.Max(-boundaries.height/2.0f, node.getY())),
-                        Mathf.Min(boundaries.length/2.0f, Mathf.Max(-boundaries.length/2.0f, node.getZ())));
-                      
-                }
+                node.setPosition(Mathf.Min(boundaries.width/2.0f, Mathf.Max(-boundaries.width/2, node.getX())),
+                    Mathf.Min(boundaries.height/2.0f, Mathf.Max(-boundaries.height/2.0f, node.getY())),
+                    Mathf.Min(boundaries.length/2.0f, Mathf.Max(-boundaries.length/2.0f, node.getZ())));
+                wasauchimmer++;      
+            }
                 
                 temperature = cool(temperature);
-            }
+            
         }
+
+        Debug.Log("Reached end of Fruchterman Reingold");
     }
 
-    public void drawGraph()
+    public List<GameObject> drawGraph()
     {
         //draw nodes
         List<GameObject> drawnNodes = new List<GameObject>();
@@ -236,6 +280,7 @@ public class GraphVisualizer : MonoBehaviour {
             GameObject newBall = Instantiate(ball, new Vector3(currentNode.getX(), currentNode.getY(), currentNode.getZ()), Quaternion.identity);
             drawnNodes.Add(newBall);
         }
+        /*
         foreach (Graph.Edge currentEdge in edges)
         {
             //maybe use GL.LINES instead...?
@@ -244,6 +289,9 @@ public class GraphVisualizer : MonoBehaviour {
             LineRenderer line = lineObject.GetComponent<LineRenderer>();
             //add more stuff?
         }
+        */
+
+        return drawnNodes;
     }
 
     //to use this, first get a list of usable neighbors
